4a140497c728e3096fdb3ed2a65b9cdac534527d
Aecor <thexenon01@protonmail.com>
2026-04-07 23:46:38 +0530

    Finishing setup

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+
+windowDimensions = (1280, 720)
+backgroundColor = pygame.Color("#bebebe")
+
+pygame.init()
+pygame.display.set_caption("Linc Up")
+running = True
+
+screen = pygame.display.set_mode(windowDimensions, pygame.RESIZABLE)
+clock = pygame.time.Clock()
+
+# Main app loop
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    deltaTime = clock.tick(60)
+    screen.fill(backgroundColor)
+    
+    # Render here
+    
+    pygame.display.flip()
+    
+pygame.quit()
