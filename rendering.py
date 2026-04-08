68c3dc4113a8bfe0773fbb08d09e8a3ea20d6f11
Aecor <thexenon01@protonmail.com>
2026-04-08 11:08:19 +0530

    Beginnings of the grid

--- rendering.py
+++ rendering.py
@@ -3,13 +3,16 @@ import pygame
 
 lines = list()
 
+scale = 10
+
 def render(screen):
     turned = False
-    for i in range(5):
-        for j in range(5):
+    width, height = screen.get_size()
+    for i in range(scale):
+        for j in range(1, scale):
             if turned:
-                pygame.draw.line(screen, "black", (0, 0), (0, 0), width=1)
+                pygame.draw.line(screen, "black", ((width/scale)*j, 0), ((width/scale)*j, 1000), width=1)
             else:
-                pygame.draw.line(screen, "black", (0, 0), (0, 0), width=1)
+                pygame.draw.line(screen, "black", (0, (height/scale)*j), (1000, (height/scale)*j), width=1)
         if i > 2:
             turned = True
